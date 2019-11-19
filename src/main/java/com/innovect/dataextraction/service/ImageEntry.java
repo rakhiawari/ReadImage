b8e62073eac6d8c959d84e0f5d75587cd5200a23
rakhiawari <rakhiawari98@gmail.com>
2019-11-19 15:15:42 +0530

    working for simple text images

--- src/main/java/com/innovect/dataextraction/service/ImageEntry.java
+++ src/main/java/com/innovect/dataextraction/service/ImageEntry.java
@@ -3,10 +3,13 @@ package com.innovect.dataextraction.service;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.util.ImageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
-
+import java.io.IOException;
 
 @Service
 public class ImageEntry {
@@ -14,12 +17,19 @@ public class ImageEntry {
 
     public  String crackImage() {
 
-        File imagePath=new File("/home/rakhi/Desktop/text.png");
-        ITesseract image = new Tesseract();
-        image.setDatapath("/home/rakhi/Spring-boot-projects/data-extraction/tessdata");
-        image.setLanguage("eng");
+        File imagePath=new File("/home/rakhi/Desktop/bill3.jpeg");
+        ITesseract imageFile = new Tesseract();
+        imageFile.setDatapath("/home/rakhi/Spring-boot-projects/data-extraction/tessdata");
+        imageFile.setLanguage("eng");
+        BufferedImage image  = null;
         try {
-            String result = image.doOCR(imagePath);
+            image = ImageIO.read(imagePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        image=ImageHelper.convertImageToGrayscale(image);
+        try {
+            String result = imageFile.doOCR(image);
             return result;
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
